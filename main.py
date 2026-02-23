b63a8601f36d0982c398781cc797418142a22cad
jwhite9387 <jwhite93@uab.edu>
2026-02-22 22:55:36 -0600

    Player turning working

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
 
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -13,11 +23,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        player.update(dt)
+
+        player.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    
 
 
 if __name__ == "__main__":
